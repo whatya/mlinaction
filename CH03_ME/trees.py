4e0f975dc3b5dd3e86bf1c1d898d6ea42bd08c3f
xg <2398772538@qq.com>
2017-12-22 16:39:51 +0800

    1、完成决策树；

--- CH03_ME/trees.py
+++ CH03_ME/trees.py
@@ -3,9 +3,10 @@
 # 以上两行代码解决Python不支持中文的问题
 
 from math import log
+import operator
 
-# 计算信息熵：熵定义为信息期望值, 被分类后的各数据之间相似程度越高（越有序）熵越高, 数据之间的
-# 相似程度越低（越杂乱、无序）熵越低
+# 计算信息熵：熵定义为信息期望值, 被分类后的各数据之间相似程度越高（越有序）熵越低, 数据之间的
+# 相似程度越低（越杂乱、无序）熵越高
 def calcShannonEnt(dataSet):
     numEntries = len(dataSet)                                                   # 获取训练数据总数
     labelCounts = {}                                                            # 记录每个标签（分类）出现的总次数如：{‘yes’:2}
@@ -60,3 +61,59 @@ def chooseBestFeatureToSplit(dataSet):
             bestInfoGain = infoGain
             bestFeature = i
     return bestFeature
+
+
+
+# 投票表决
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys(): classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(),\
+    key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+# 创建树
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet,\
+        bestFeat, value), subLabels)
+    return myTree
+
+
+def classify(inputTree, featLabels, testVec):
+    firstStr = inputTree.keys()[0]
+    secondDict = inputTree[firstStr]
+    featIndex = featLabels.index(firstStr)
+    for key in secondDict.keys():
+        if testVec[featIndex] == key:
+            if type(secondDict[key]).__name__=='dict':
+                classLabel = classify(secondDict[key],featLabels,testVec)
+            else: classLabel = secondDict[key]
+    return classLabel
+
+#树持久化
+def storeTree(inputTree,filename):
+    import pickle
+    fw = open(filename,'w')
+    pickle.dump(inputTree,fw)
+    fw.close()
+
+def grabTree(filename):
+    import pickle
+    fr = open(filename)
+    return pickle.load(fr)
